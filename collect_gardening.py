7a462da7f1105b9f7768ad9f304f76a712f45e07
nicolerabjohn <nicolerabjohn@gmail.com>
2024-09-22 18:40:26 -0400

    Update gardening

--- collect_gardening.py
+++ collect_gardening.py
@@ -2,6 +2,7 @@ import subprocess
 import update_gardening
 
 def run_command(command):
+    print("Nicole")
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode != 0:
         print(f"Error: {result.stderr}")
